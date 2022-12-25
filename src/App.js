bd0b2e0928e4d62e657331032c63669fdd48b296
jae <albertforus@gmail.com>
2022-12-24 22:37:06 -0500

    restructure search result, add movie year

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import ClearIcon from '@mui/icons-material/Clear';
 function App() {
   const [inputText, setInputText] = useState("");
   const [searchRes, setSearchRes] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isResDiv, setResDiv] = useState(false) ;
+  const [isNominateList, setNominateList] = useState([]);
 
   const inputHandler = (e) => {
     var lowerCase = e.target.value.toLowerCase();
@@ -18,14 +19,13 @@ function App() {
 
   useEffect(() => {
     const fetchMovies = async () => {
-      setIsLoading(true);
       const res = await fetch('http://www.omdbapi.com/?' + new URLSearchParams(
                         {apikey: config.OMDB_KEY,
                         s: `${inputText}`}));
       const data = await res.json();
       if (data.hasOwnProperty('Search')){
-        setSearchRes(data['Search'].map(elem => elem.Title));
-        setIsLoading(false);
+        setSearchRes(data['Search'].map(elem => ({Title: elem.Title, Year: elem.Year})));
+        setResDiv(true);
       }
     };
     fetchMovies();
@@ -33,39 +33,39 @@ function App() {
 
   return (
     <Container maxWidth="md">
-      <Box sx={{pt: 5, pb: 2}}>
-            <TextField 
-                      fullWidth
-                      id="standard-basic" 
-                      value = {inputText}
-                      onChange={inputHandler}
-                      label="Movie titles" 
-                      variant="outlined"
-                      InputProps={{
-                        endAdornment: inputText ? (
-                          <IconButton 
-                              size="small"
-                              onClick={() => setInputText("")}
-                              aria-label="clear">
-                              <ClearIcon/>
-                          </IconButton>
-                        ) : undefined
-                      }}
-            />           
-      </Box>
-      <Paper>
-        <Typography variant="h6" gutterBottom component="div" sx={{ p: 2, pb: 0 }}>
-          Search Results
+        <Typography variant="subtitle1" gutterBottom component="div" sx={{pt: 2}}>
+        The Shoppies
         </Typography>
-        <Divider />
-      
-      {isLoading? (
-            <div>Loading...</div>
-          ) : (
-            <MovieList input = {inputText} 
-            titleList = {searchRes}/>
-          )}
-      </Paper>
+        <Typography variant="subtitle2" component="div">
+        Shopify 2021 Summer Internship Challenge
+        </Typography>
+        <Box sx={{pt: 5, pb: 2}}>
+              <TextField 
+                        fullWidth
+                        id="standard-basic" 
+                        value = {inputText}
+                        onChange={inputHandler}
+                        label="Movie titles" 
+                        variant="outlined"
+                        InputProps={{
+                          endAdornment: inputText ? (
+                            <IconButton 
+                                size="small"
+                                onClick={() => {setInputText("");
+                                                setResDiv(false)}}
+                                aria-label="clear">
+                                <ClearIcon/>
+                            </IconButton>
+                          ) : undefined
+                        }}
+              />           
+        </Box>
+    
+        { isResDiv && (
+        <MovieList input = {inputText} 
+        titleList = {searchRes}/>
+        )}
+
     </Container>
       
   );
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,31 +3,29 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import { Paper, Divider, Typography } from '@mui/material';
 
 
 function MovieList(props) {
-    // const filteredData = props.titleList.filter((elem) => {
-    //     if (props.input === '') {
-    //         return elem;
-    //     } else {
-    //         return elem.Title.toLowerCase().includes(props.input);
-    //     }
-    // })
-    //console.log(props.titleList)
 
     return (
-            <List>
+        <Paper>
+        <Typography variant="h6" gutterBottom component="div" sx={{ p: 2, pb: 0 }}>
+          Search Results
+        </Typography>
+        <Divider />
+        <List>
             {props.titleList.map((item, index) => (
                 <ListItem 
                 key={index}>
                     <ListItemButton component="a" href="#simple-list">
-                        <ListItemText primary = {item}/>
+                        <ListItemText primary = {`${item.Title} (${item.Year})`}/>
                     </ListItemButton>
                 </ListItem>
             ))}
            
             </List>
-        
+        </Paper> 
     )
 }
 
