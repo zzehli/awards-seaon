ad1a13357c7d04506b9d8a8b94f58aa10b511119
jae <albertforus@gmail.com>
2022-12-28 16:59:23 -0500

    detail page on search results and nominate button

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,48 @@
-import { React } from "react";
+import { React, useCallback, useState } from "react";
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
-import { Paper, Divider, Typography, IconButton } from '@mui/material';
+import { Paper, Divider, Typography, IconButton, ListItemButton } from '@mui/material';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
+import MovieDetail from "./MovieDetail";
+import { config } from '../config';
+
 
 function MovieList(props) {
+    const [openDetail, setOpenDetail] = useState(false);
+    const [detail, setDetail] = useState([]);
+    
+    const handleCloseDetail = () => {
+        setOpenDetail(false);
+    }
+    // https://bobbyhadz.com/blog/react-fetch-data-on-button-click
+    const fetchMovie = async (id) => {
+        try{
+            
+            const res = await fetch('http://www.omdbapi.com/?' + new URLSearchParams(
+                                                            {apikey: config.OMDB_KEY,
+                                                            i: `${id}`,
+                                                            plot: 'full'}));
+                const data = await res.json();
+                if (data.hasOwnProperty('Title')){
+                //setSearchRes(data['Search'].map(elem => ({Title: elem.Title, Year: elem.Year, imdbID: elem.imdbID})));
+                //setResDiv(true);
+                setDetail({title: data.Title,
+                            year: data.Year,
+                            runtime: data.Runtime,
+                            director: data.Director,
+                            actors: data.Actors,
+                            plot: data.Plot,
+                            poster: data.Poster,
+                            id: data.imdbID})
+                }
+                setOpenDetail(true);
+    
+          } catch (err) {
+            console.log(err)
+          }
+    }
+   
 
     return (
         <Paper>
@@ -23,18 +60,21 @@ function MovieList(props) {
                                 disabled={
                                     props.nominationList.length > 4 ||
                                     props.nominationList.some( one => one.imdbID === item.imdbID)
-                                  }
-                    >
-                        <AddCircleIcon
-                                        sx={{ fontSize: 30 }}/>
+                                  }>
+                        <AddCircleIcon sx={{ fontSize: 30 }}/>
                     </IconButton>
                 }>
-
-                    <ListItemText primary = {`${item.Title} (${item.Year})`}/>
+                    <ListItemButton onClick={() => fetchMovie(item.imdbID)}>
+                        <ListItemText primary = {`${item.Title} (${item.Year})`}/>
+                    </ListItemButton>
                 </ListItem>
             ))}
            
-            </List>
+        </List>
+        {Object.keys(detail).length !== 0 && <MovieDetail open={openDetail} 
+                                                    handleClose={handleCloseDetail}
+                                                    detail={detail}
+                                                    nominate={props.nominationHandler}/>}
         </Paper> 
     )
 }
